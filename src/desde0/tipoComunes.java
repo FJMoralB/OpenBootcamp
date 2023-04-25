77733be277486fe1720c27fe814796a6b29f5c63
FJMoralB <moralbermudez@gmail.com>
2023-04-25 22:06:42 +0200

    Initial commit

---
+++ src/desde0/tipoComunes.java
@@ -0,0 +1,16 @@
+package desde0;
+
+public class tipoComunes {
+    public static void main(String[] args) {
+        int a = 10;
+        long b = 20;
+        double c = 30.5;
+        boolean d = true;
+        String e = "hola";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
